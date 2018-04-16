61fb34a6741aa5e694ac65606e41a3b96364d121
eldare <eldare@gmail.com>
2018-04-16 22:41:21 +0300

    Base

---
+++ MacHostsSwitcher/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MacHostsSwitcher
+//
+//  Created by Eldar on 16/04/2018.
+//  Copyright © 2018 thedp. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
